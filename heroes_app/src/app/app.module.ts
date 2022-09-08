b86d328faef68c9c69752b5ac7d25ea8cbb8cec0
Susmi Benny <susmibenny1999@gmail.com>
2022-09-08 12:06:50 +0100

    Add edit input field for hero name

--- heroes_app/src/app/app.module.ts
+++ heroes_app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,8 +12,9 @@ import { HeroesComponent } from './heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
